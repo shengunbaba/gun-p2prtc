5fe43adb1aba9ff92f815e27a1ddcb574b278674
shengunbaba <shengunbaba@gmail.com>
2021-12-05 15:17:36 +0800

    complete

--- src/peer2peer.js
+++ src/peer2peer.js
@@ -1,91 +1,107 @@
-import {EventDispatcher, createShortId} from './utils'
+import { EventDispatcher, createShortId } from './utils'
 import defaultCon from './default.config'
 
 
 export default class Peer2peer extends EventDispatcher {
     constructor(opts) {
         super(opts)
-        this._options = {...defaultCon, ...opts};
-        const {rtcConfiguration, localStream} = this._options;
-        this._pc = new RTCPeerConnection(rtcConfiguration);
-        this.id = createShortId();
-
-
+        this._options = { ...defaultCon, ...opts }
+        const { rtcConfiguration, localStream } = this._options
+        this._pc = new RTCPeerConnection(rtcConfiguration)
+        this.id = createShortId()
+        this._remoteStream = null
         this._pc.oniceconnectionstatechange = (e) => {
             console.log(e, this._pc.iceConnectionState)
-        };
+        }
         this._pc.onicegatheringstatechange = (e) => {
             console.log(e, this._pc.iceConnectionState)
-        };
+        }
         this._pc.onconnectionstatechange = (e) => {
             console.log(e, this._pc.connectionState)
-        };
+        }
         this._pc.addEventListener('signalingstatechange', (e) => {
             console.log(e, this._pc.signalingState)
-        });
+        })
         this._pc.addEventListener('icecandidate', event => {
             console.log(event)
-        });
+            this._onicecandidate(event)
+        })
 
         this._pc.addEventListener('track', event => {
-            console.log(event)
+            this._onRemoteTrack(event)
         })
         if (localStream && typeof localStream instanceof MediaStream) {
             this._addTrack()
         } else {
-            this._createLocalStream();
+            this._createLocalStream()
         }
     }
 
+    _onRemoteTrack(event) {
+        console.log(event)
+        const remoteStream = event.streams[0]
+        if (remoteStream && this._remoteStream!==remoteStream) {
+            this.dispatchEvent({ type: 'remoteStream', remoteStream })
+        }
+    }
+
+    _onicecandidate(event) {
+        this.dispatchEvent(event)
+    }
+
     _addTrack() {
-        const {localStream} = this._options
-        localStream.getTracks().forEach(track => this._pc.addTrack(track, localStream));
+        const { localStream } = this._options
+        localStream.getTracks().forEach(track => this._pc.addTrack(track, localStream))
     }
 
     _createLocalStream() {
-        const {constraints, initiator} = this._options;
+        const { constraints, initiator } = this._options
         navigator.mediaDevices.getUserMedia(constraints).then(stream => {
-            this._options.localStream = stream;
-            this.dispatchEvent({type: 'localStream', stream})
+            this._options.localStream = stream
+            this.dispatchEvent({ type: 'localStream', stream })
             this._addTrack()
             if (initiator) {
                 this._createOffer()
             }
         }, error => {
-            this.dispatchEvent({type: 'error', error})
+            this.dispatchEvent({ type: 'error', error })
         })
     }
 
     _createOffer() {
-        const {offerOptions} = this._options;
+        const { offerOptions } = this._options
         this._pc.createOffer(offerOptions).then(offer => {
             this._pc.setLocalDescription(offer).then(() => {
                 this.dispatchEvent(offer)
             }).catch(error => {
-                this.dispatchEvent({type: 'error', error})
-            });
+                this.dispatchEvent({ type: 'error', error })
+            })
         })
     }
 
     _createAnswer() {
-        const {answerOptions} = this._options;
+        const { answerOptions } = this._options
         this._pc.createAnswer(answerOptions).then(answer => {
             this._pc.setLocalDescription(answer).then(() => {
                 this.dispatchEvent(answer)
-            }).catch(error => this.dispatchEvent({type: 'error', error}));
-        }).catch(error => this.dispatchEvent({type: 'error', error}));
+            }).catch(error => this.dispatchEvent({ type: 'error', error }))
+        }).catch(error => this.dispatchEvent({ type: 'error', error }))
     }
 
     signal(data) {
         if (data.sdp) {
             this._pc.setRemoteDescription(data).then(() => {
-                if (data.type === 'offer') this._createAnswer();
+                if (data.type==='offer') this._createAnswer()
             }).catch(error => {
-                this.dispatchEvent({type: 'error', error})
-            });
+                this.dispatchEvent({ type: 'error', error })
+            })
+        }
+
+        if (data.candidate) {
+            this._pc.addIceCandidate(data.candidate)
         }
     }
 }
 
 
-window.Peer2peer = Peer2peer;
+window.Peer2peer = Peer2peer
